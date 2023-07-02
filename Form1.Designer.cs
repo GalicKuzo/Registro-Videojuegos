8fea70cfa9b541aa480cb90a1b9124617bcb91c3
Sandro <fernando.meza.perales@gmail.com>
2023-07-01 21:27:33 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Registro_Videojuegos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.cmbox_resultado = new System.Windows.Forms.ComboBox();
+            this.btn_registrar = new System.Windows.Forms.Button();
+            this.dgv_Datos = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_puntos = new System.Windows.Forms.Label();
+            this.nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.resultado = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.puntos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Datos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_registrar);
+            this.groupBox1.Controls.Add(this.cmbox_resultado);
+            this.groupBox1.Controls.Add(this.txt_nombre);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(400, 242);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Registro de Videojuego";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Resultado";
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(93, 60);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(288, 26);
+            this.txt_nombre.TabIndex = 3;
+            // 
+            // cmbox_resultado
+            // 
+            this.cmbox_resultado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbox_resultado.FormattingEnabled = true;
+            this.cmbox_resultado.Items.AddRange(new object[] {
+            "Victoria",
+            "Derrota"});
+            this.cmbox_resultado.Location = new System.Drawing.Point(107, 100);
+            this.cmbox_resultado.Name = "cmbox_resultado";
+            this.cmbox_resultado.Size = new System.Drawing.Size(274, 28);
+            this.cmbox_resultado.TabIndex = 4;
+            // 
+            // btn_registrar
+            // 
+            this.btn_registrar.Location = new System.Drawing.Point(21, 149);
+            this.btn_registrar.Name = "btn_registrar";
+            this.btn_registrar.Size = new System.Drawing.Size(360, 40);
+            this.btn_registrar.TabIndex = 5;
+            this.btn_registrar.Text = "Registrar";
+            this.btn_registrar.UseVisualStyleBackColor = true;
+            this.btn_registrar.Click += new System.EventHandler(this.btn_registrar_Click);
+            // 
+            // dgv_Datos
+            // 
+            this.dgv_Datos.AllowUserToAddRows = false;
+            this.dgv_Datos.AllowUserToDeleteRows = false;
+            this.dgv_Datos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_Datos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Datos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nombre,
+            this.resultado,
+            this.puntos});
+            this.dgv_Datos.Location = new System.Drawing.Point(423, 21);
+            this.dgv_Datos.Name = "dgv_Datos";
+            this.dgv_Datos.ReadOnly = true;
+            this.dgv_Datos.RowHeadersVisible = false;
+            this.dgv_Datos.RowHeadersWidth = 80;
+            this.dgv_Datos.RowTemplate.Height = 24;
+            this.dgv_Datos.Size = new System.Drawing.Size(510, 190);
+            this.dgv_Datos.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(430, 225);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(198, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Puntos Acumulados: ";
+            // 
+            // lbl_puntos
+            // 
+            this.lbl_puntos.AutoSize = true;
+            this.lbl_puntos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_puntos.Location = new System.Drawing.Point(628, 225);
+            this.lbl_puntos.Name = "lbl_puntos";
+            this.lbl_puntos.Size = new System.Drawing.Size(0, 25);
+            this.lbl_puntos.TabIndex = 7;
+            // 
+            // nombre
+            // 
+            this.nombre.HeaderText = "Nombre";
+            this.nombre.MinimumWidth = 6;
+            this.nombre.Name = "nombre";
+            this.nombre.ReadOnly = true;
+            // 
+            // resultado
+            // 
+            this.resultado.HeaderText = "Resultado";
+            this.resultado.MinimumWidth = 6;
+            this.resultado.Name = "resultado";
+            this.resultado.ReadOnly = true;
+            // 
+            // puntos
+            // 
+            this.puntos.HeaderText = "Puntos Obtenidos";
+            this.puntos.MinimumWidth = 6;
+            this.puntos.Name = "puntos";
+            this.puntos.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(945, 274);
+            this.Controls.Add(this.lbl_puntos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dgv_Datos);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Datos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cmbox_resultado;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_registrar;
+        private System.Windows.Forms.DataGridView dgv_Datos;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbl_puntos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn resultado;
+        private System.Windows.Forms.DataGridViewTextBoxColumn puntos;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Registro_Videojuegos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lbl_puntos.Text = "0";
+        }
+        int score = 0;
+        private void btn_registrar_Click(object sender, EventArgs e)
+        {
+            lbl_puntos.Text = Convert.ToString(score);
+            if (txt_nombre.Text.Trim() == "" || cmbox_resultado.SelectedItem == null)
+            {
+                MessageBox.Show("Debe llenar los espacios en blanco antes de continuar :)", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            int n = dgv_Datos.Rows.Add();
+            dgv_Datos.Rows[n].Cells[0].Value = Convert.ToString(txt_nombre.Text).ToUpper();
+            dgv_Datos.Rows[n].Cells[1].Value = Convert.ToString(cmbox_resultado.Text).ToUpper();
+            if (cmbox_resultado.Text == "Victoria")
+            {
+                dgv_Datos.Rows[n].Cells[2].Value = "100";
+                score = score + 100;
+                lbl_puntos.Text = Convert.ToString(score);
+            }
+            if (cmbox_resultado.Text == "Derrota")
+            {
+                dgv_Datos.Rows[n].Cells[2].Value = "-20";
+                score = score - 20;
+                lbl_puntos.Text = Convert.ToString(score);
+            }
+        }
+    }
+}
